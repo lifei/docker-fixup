5ca5be4b6f15e2f14c41af0c12f4cb841817b7a5
软件工程师李飛 <lifei@7v1.net>
2015-09-13 20:55:55 +0800

    Create fixup.sh

---
+++ fixup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sed -i 's/http:\/\/archive\.ubuntu\.com\/ubuntu\//http:\/\/mirrors.sohu.com\/ubuntu\//' /etc/apt/sources.list && \
+sed -i 's/deb-src/# deb-src/' /etc/apt/sources.list && \
+mkdir /tmp/fixup && cd /tmp/fixup && \
+curl -fSL -o baseimage-docker.zip "https://github.com/phusion/baseimage-docker/archive/master.zip" && \
+unzip baseimage-docker.zip && \
+mv baseimage-docker-master/image /bd_build && \
+/bd_build/prepare.sh && \
+/bd_build/system_services.sh && \
+/bd_build/utilities.sh && \
+/bd_build/cleanup.sh
+rm -rf /tmp/fixup
