a643c95c153fc4b24f2fe1364e91649b3e4a77c9
软件工程师李飛 <lifei@7v1.net>
2015-09-28 23:59:28 +0800

    Update fixup.sh

--- fixup.sh
+++ fixup.sh
@@ -6,8 +6,8 @@ mkdir /tmp/fixup && cd /tmp/fixup && \
 curl -fSL -o baseimage-docker.tar.gz "https://github.com/phusion/baseimage-docker/archive/master.tar.gz" && \
 tar zxf baseimage-docker.tar.gz && \
 mv baseimage-docker-master/image /bd_build && \
-/bd_build/prepare.sh && \
-/bd_build/system_services.sh && \
-/bd_build/utilities.sh && \
+/bd_build/prepare.sh \
+/bd_build/system_services.sh \
+/bd_build/utilities.sh \
 /bd_build/cleanup.sh
 rm -rf /tmp/fixup
