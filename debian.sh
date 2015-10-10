e9f3c6126544873f851b26f6e34dd85bddbef688
lifei <lifei.vip@outlook.com>
2015-10-10 22:37:10 +0800

    fix debian

--- debian.sh
+++ debian.sh
@@ -6,8 +6,7 @@ mkdir -p /tmp/fixup && \
 curl -fSL -o /tmp/fixup/master.tar.gz "https://github.com/lifei/docker-fixup/archive/master.tar.gz" && \
 tar zxf /tmp/fixup/master.tar.gz -C /tmp/fixup && \
 mv /tmp/fixup/docker-fixup-master/scripts /bd_build && \
-find /bd_build -name '*.sh' | xargs chmod a+x && \
-/bd_build/prepare-debian.sh
+/bd_build/prepare-debian.sh && \
 /bd_build/system_services.sh && \
 /bd_build/utilities.sh && \
 /bd_build/cleanup.sh && \
