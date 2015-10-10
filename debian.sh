28948c5457504494681a7e5997430af36fe6d46f
lifei <lifei.vip@outlook.com>
2015-10-10 18:27:42 +0800

    split script for ubuntu and debian

--- debian.sh
+++ debian.sh
@@ -6,7 +6,7 @@ mkdir /tmp/fixup && cd /tmp/fixup && \
 curl -fSL -o master.tar.gz "https://github.com/lifei/docker-fixup/archive/master.tar.gz" && \
 tar zxf master.tar.gz && \
 mv docker-fixup-master/scripts /bd_build && \
-/bd_build/prepare.sh
+/bd_build/prepare-debian.sh
 /bd_build/system_services.sh && \
 /bd_build/utilities.sh && \
 /bd_build/cleanup.sh && \
