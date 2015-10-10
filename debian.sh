754227a4c1dc6c0a9aec33ee153615dd412a4983
lifei <lifei.vip@outlook.com>
2015-10-10 22:18:37 +0800

    fix path error

--- debian.sh
+++ debian.sh
@@ -2,10 +2,10 @@
 
 sed -i 's/httpredir.debian.org/mirrors.163.com/' /etc/apt/sources.list
 sed -i 's/deb-src/# deb-src/' /etc/apt/sources.list
-mkdir /tmp/fixup && \
-curl -fsSL -o /tmp/fixup/master.tar.gz "https://github.com/lifei/docker-fixup/archive/master.tar.gz" && \
+mkdir -p /tmp/fixup && \
+curl -fSL -o /tmp/fixup/master.tar.gz "https://github.com/lifei/docker-fixup/archive/master.tar.gz" && \
 tar zxf /tmp/fixup/master.tar.gz -C /tmp/fixup && \
-mv docker-fixup-master/scripts /bd_build && \
+mv /tmp/fixup/docker-fixup-master/scripts /bd_build && \
 /bd_build/prepare-debian.sh
 /bd_build/system_services.sh && \
 /bd_build/utilities.sh && \
